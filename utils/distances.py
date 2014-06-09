12aa63ec1ae883b698453c495e8ca38bfbe06812
Vincent Michel <vincent.michel@logilab.fr>
2014-06-09 13:51:36 +0000

    [distance] Add safety belt on geographical distance units, closes #248555

--- utils/distances.py
+++ utils/distances.py
@@ -318,6 +318,7 @@ def geographical(pointa, pointb, in_radians=False, planet_radius=6371009,
 
         - `units` can be 'm' (meters) or 'km' (kilometers)
     """
+    assert units in ('m', 'km')
     pointa = (float(pointa[0]), float(pointa[1]))
     pointb = (float(pointb[0]), float(pointb[1]))
 
