de9b4ddd485dd8ad80355782fe73c4dcb98eb812
Adrien Di Mascio <Adrien.DiMascio@logilab.fr>
2014-11-25 18:29:01 +0100

    prepare 0.7.1

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -23,7 +23,7 @@ import sys
 distname = 'nazca'
 modname = 'nazca'
 
-numversion = (0, 7, 0)
+numversion = (0, 7, 1)
 version = '.'.join([str(num) for num in numversion])
 
 license = 'LGPL' # 2.1 or later
